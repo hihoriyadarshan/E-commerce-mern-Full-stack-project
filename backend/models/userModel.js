ee5b0233e2e903d35b8faf65070a21bb62ba43a3
hihoriyadarshan <112104707+hihoriyadarshan@users.noreply.github.com>
2023-07-29 13:40:15 +0530

    registration and password hash successfully

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validators = require("validator");
+const bcryptjs = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -7,40 +8,47 @@ const userSchema = new mongoose.Schema({
         required: [true, "Please Enter Your Name"],
         maxLength: [30, "Name cannot exceed 30 characters"],
         minLength: [4, "Name should have more than 4 characters"],
-      },
-      email: {
+    },
+    email: {
         type: String,
         required: [true, "Please Enter Your Email"],
         unique: true,
-        validate: [validator.isEmail, "Please Enter a valid Email"],
-      },
-      password: {
+        validate: [validators.isEmail, "Please Enter a valid Email"],
+    },
+    password: {
         type: String,
         required: [true, "Please Enter Your Password"],
         minLength: [8, "Password should be greater than 8 characters"],
         select: false,
-      },
-      avatar: {
+    },
+    avatar: {
         public_id: {
-          type: String,
-          required: true,
+            type: String,
+            required: true,
         },
         url: {
-          type: String,
-          required: true,
+            type: String,
+            required: true,
         },
-      },
-      role: {
+    },
+    role: {
         type: String,
         default: "user",
-      },
-      createdAt: {
+    },
+    createdAt: {
         type: Date,
         default: Date.now,
-      },
-    
-      resetPasswordToken: String,
-      resetPasswordExpire: Date,
-    });
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+});
+
+userSchema.pre("save", async function(next){
+
+  if(!this.isModified("password")){
+        next();
+  }
+  this.password = await bcrypt.hash(this.password,10)
+})
 
 module.exports = mongoose.model("User", userSchema);
