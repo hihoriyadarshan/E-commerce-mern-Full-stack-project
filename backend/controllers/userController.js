328321f3fcce689817dce8617011b82a7c06879a
hihoriyadarshan <112104707+hihoriyadarshan@users.noreply.github.com>
2023-08-01 18:34:23 +0530

    Login Error Fix

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,7 @@
 const ErrorHander = require("../utils/errorhander");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const User = require("../models/userModel");
-
+const sendToken = require("../utils/jwtToken");
 
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
 
@@ -15,46 +15,31 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
             }
         });
 
-        const token = user.getJWTToken();
-
-        res.status(201).json({
-            success:true,
-            user,
-            token,
-
-        });
+        sendToken(user,201,res);
 });
 
 // Login USER
 
-exports.loginUser = catchAsyncErrors(async(req,res,next) => {
-
-    const{email,password} = req.body;
-
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+    const { email, password } = req.body;
+  
     // checking if user has given password and email both
-
-    if(!email || !password){
-        return next(new ErrorHander("Please Enter Email & Password", 400))
-
+  
+    if (!email || !password) {
+      return next(new ErrorHander("Please Enter Email & Password", 400));
     }
-
-    const user = await User.findOne({email}).select("+ password");
-
-    if(!user){
-        return next(new ErrorHander("Invalid email or Password",401)); //email not match
+  
+    const user = await User.findOne({ email }).select("+password");
+  
+    if (!user) {
+      return next(new ErrorHander("Invalid email or password", 401));
     }
-
-    const isPasswordMatched = user.comparePassword(password);
-
-    if(!isPasswordMatched){
-        return next(new ErrorHander("Invalid email or Password",401)); // password not match
+  
+    const isPasswordMatched = await user.comparePassword(password);
+  
+    if (!isPasswordMatched) {
+      return next(new ErrorHander("Invalid email or password", 401));
     }
-
-    const token = user.getJWTToken();
-
-        res.status(201).json({
-            success:true,
-            user,
-            token,
-    });
-}); 
+  
+    sendToken(user, 200, res);
+  });
