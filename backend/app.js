08e8194e9d3633de67b0133e012e3e1703a28392
hihoriyadarshan <112104707+hihoriyadarshan@users.noreply.github.com>
2023-12-21 13:00:53 +0530

    backend order crud create

--- backend/app.js
+++ backend/app.js
@@ -11,9 +11,11 @@ app.use(cookieParser());
 // Route import
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
+const order = require("./routes/orderRoutes");
 
 app.use("/api/v1", product);
 app.use("/api/v1", user);
+app.use("/api/v1", order);
 
 // middleware for error
 app.use(errorMiddleware);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,24 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Header from "./component/layout/Header/Header";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Import 'Routes'
-import WebFont from "webfontloader";
-import Footer from "./component/layout/Footer/Footer";
-import Home from "./component/Home/Home";
-import LoginSignUp from './component/User/LoginSignUp';
 
 function App() {
-  React.useEffect(() => {
-    WebFont.load({
-      google: {
-        families: ["Roboto", "Droid Sans", "Chilanka"],
-      },
-    });
-  }, []);
-
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginSignUp />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
